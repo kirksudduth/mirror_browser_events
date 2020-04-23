b8b03a08646d0adc264a95c1ea19c199798d1483
Kirk Sudduth <kirksudduth@gmail.com>
2020-04-23 16:28:30 -0500

    did flexbox to make the text wrap in the box

--- mirror.js
+++ mirror.js
@@ -1,5 +1,5 @@
 let outputField = document.getElementsByClassName("my-articles");
-
+let inputField = document.getElementById("message");
 inputField.addEventListener("keyup", function () {
   for (item of outputField) {
     item.textContent = event.target.value;
